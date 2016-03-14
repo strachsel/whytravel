cbeb7d89b583e57900c1ec96d25c309a5d61e44c
Sonya Trachsel <sofyatrachsel@gmail.com>
2016-03-14 09:50:57 -0700

    added a controller for converting the exchange rate from rubles to dollars and displaying the dollar amount in the view

---
+++ js/controllers/CurrencyCtrl.js
@@ -0,0 +1,43 @@
+myApp.controller('CurrencyCtrl', function($scope) {
+    $scope.money = 75;
+    $scope.uber = 700;
+    $scope.airbnb = 3500;
+    $scope.bar = 500;
+    $scope.dinner = 1500;
+    $scope.museum = 700;
+    $scope.subway = 35;
+})
+
+myApp.filter('currency', function() {
+    var defaultCurrency = '₽';
+    return function(input, currencySymbol) {
+      var out = "";
+      currencySymbol = currencySymbol || defaultCurrency;
+
+        switch(currencySymbol) {
+            case '$':
+                out = 0.014 * input; // google
+                break;
+
+            default:
+                out = input;
+        }
+
+      return currencySymbol + out + ' ';
+    }
+  });
+
+  myApp.filter('noFractionCurrency',
+  [ '$filter', '$locale',
+  function(filter, locale) {
+    var currencyFilter = filter('currency');
+    var formats = locale.NUMBER_FORMATS;
+    return function(amount, currencySymbol) {
+      var value = currencyFilter(amount, currencySymbol);
+      var sep = value.indexOf(formats.DECIMAL_SEP);
+      if(amount >= 0) {
+        return value.substring(0, sep);
+      }
+      return value.substring(0, sep) + ')';
+    };
+  } ]);
--- js/controllers/ForecastCtrl.js
+++ js/controllers/ForecastCtrl.js
@@ -5,7 +5,7 @@ myApp.controller('ForecastCtrl', ['$scope', 'forecast', function($scope, forecas
          });
        }
 
-       fetchWeather('2346910'); // moscow woeid. tula - 2124001
+       fetchWeather('2346910'); // moscow woeid. tula - 2124001 ,, portland - 2475687
 
        $scope.findWeather = function(id) {
        $scope.place = '';
