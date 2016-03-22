800fa9f727747cbc5572e26ee174cc537dc7477f
Sonya Trachsel <sofyatrachsel@gmail.com>
2016-03-22 13:45:45 -0700

    updated the current styles, including responsive design

--- server/public/js/controllers/MainController.js
+++ server/public/js/controllers/MainController.js
@@ -2,5 +2,5 @@ myApp.controller('MainController', ['$scope', function($scope) {
   $scope.country = 'Russia';
   $scope.currency = 'Rubles';
   $scope.language = 'Russian';
-  $scope.about = 'Ever wonder why go to Russia? Because Russia is amazing! Besides bears playing on balalayka, endless winter and vodka pouring out of faucets it\'s very cheap to go.'
+  $scope.about = 'Ever wonder why go to Russia? Because Russia is amazing! Besides bears playing balalaikas, endless winter and vodka pouring out of faucets it\'s very cheap to go.'
 }]);
